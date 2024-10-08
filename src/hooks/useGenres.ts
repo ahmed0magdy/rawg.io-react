a1784a27a88fefe40c148031ece25687cc5b2b4a
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2024-10-08 17:42:55 +0300

    useQuery with genres & platforms

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,3 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../Services/api-client";
+import { FetchResponse } from "./useData";
 import genres from "../data/genres";
 export interface Genre {
   id: number;
@@ -6,6 +9,14 @@ export interface Genre {
 }
 
 // const useGenres = () => useData<Genre>("/genres");
-const useGenres = () => ({ data: genres, error: null, isLoading: false });
+// const useGenres = () => ({ data: genres, error: null, isLoading: false });
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: genres.length, results: genres },
+  });
 
 export default useGenres;
