969fd477c9a4f17cfe8569f6e12b9f8774114836
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2024-10-10 22:04:05 +0300

    refactor: entity interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -6,12 +6,12 @@ import {
   FaWindows,
   FaXbox,
 } from "react-icons/fa";
-import { Platform } from "../hooks/usePlatforms";
 import { HStack, Icon } from "@chakra-ui/react";
 import { SiNintendo } from "react-icons/si";
 import { MdPhoneIphone } from "react-icons/md";
 import { BsGlobe } from "react-icons/bs";
 import { IconType } from "react-icons";
+import { Platform } from "../entities/Platform";
 
 interface Props {
   platforms: Platform[];
